229710afc84239abd29e89342f6c5dd367803735
박준서 <jswing5267@naver.com>
2023-10-03 16:15:49 +0900

    feat : 네비게이션 바 퍼블리싱 완료

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,5 +1,55 @@
+import styled from "styled-components";
+import NavBar from "../components/NavBar";
+import clock from "../assets/Schedule.png";
+
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #121212;
+`;
+
+const Header = styled.div`
+  width: 100%;
+  height: 51px;
+  position: absolute;
+  top: 66px;
+  display: flex;
+  flex-direction: row;
+`;
+
+const NewImg = styled.img`
+  width: 30px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NewWord = styled.p`
+  display: flex;
+  width: 211px;
+  height: 49px;
+  flex-direction: column;
+  justify-content: center;
+  color: #959595;
+  text-align: center;
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
+
 function Main() {
-  return <div>Main Page</div>;
+  return (
+    <Background>
+      <NavBar />
+      <Header>
+        <NewImg src={clock} />
+        <NewWord>최신</NewWord>
+      </Header>
+    </Background>
+  );
 }
 
 export default Main;
