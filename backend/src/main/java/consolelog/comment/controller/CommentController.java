192d092b740b852f7513887633c848c4611965f7
fallkim <fs728kimgc@naver.com>
2023-10-15 18:57:37 +0900

    feat : commet 구현중

--- backend/src/main/java/consolelog/comment/controller/CommentController.java
+++ backend/src/main/java/consolelog/comment/controller/CommentController.java
@@ -1,31 +1,52 @@
 package consolelog.comment.controller;
 
+import consolelog.comment.dto.CommentResponse;
 import consolelog.comment.service.CommentService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
+@RestController
 public class CommentController {
 
     private final CommentService commentService;
 
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
-
-
     }
 
-    @PostMapping("post/api/v1/comment")
+    //댓글 추가
+    @PostMapping("api/v1/comment")
     public ResponseEntity<Void> addComment(@PathVariable(name = "id") Long postId,
                                            @Valid @RequestBody NewCommentRequest newCommentRequest,
                                            @Login AuthInfo authInfo) {
         Long commentId = commentService.addComment(postId, newCommentRequest, authInfo);
-        return ResponseEntity.created(URI.create("/comment" + commentId)).build();
+        return ResponseEntity.created(URI.create("/comment/" + commentId)).build();
     }
 
-    @PostMapping
+    //대댓글
+    @PostMapping("api/v1/comment/reply")
+    public ResponseEntity<Void> addReply(@PathVariable(name = "id") Long CommentId,
+                                         @Valid @RequestBody NewReplyRequest newReplyRequest,
+                                         @Login AuthInfo authInfo) {
+        Long replyId = commentService.addReply(commentId, newReplyRequest, authInfo);
+        return ResponseEntity.created(URI.create("/comments/" + replyId).build());
+    }
+
+
+    @GetMapping("post/api/v1/comment")
+    public ResponseEntity<CommentResponse> findComments(@PathVariable(name = "id") Long postId,
+                                                        @Login AuthInfo authInfo) {
+        commentService.deleteComment(commentId, authInfo);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/api/v1/comment&post_id=1")
+    public ResponseEntity<Void> deleteComment(@PathVariable(name = "id") Long commentId,
+                                              @Login AuthInfo authInfo) {
+        commentService.deleteComment(commentId, authInfo);
+        return ResponseEntity.noContent().build();
+    }
 
 }
