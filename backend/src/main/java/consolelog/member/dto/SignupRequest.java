4f5e277d8bddd447334d1fcc9a01579a8ac9e5c4
Kiminni <imk0980@gmail.com>
2023-10-17 00:10:53 +0900

    fix: member 테이블이 적용되지 않는 오류 수정

--- backend/src/main/java/consolelog/member/dto/SignupRequest.java
+++ backend/src/main/java/consolelog/member/dto/SignupRequest.java
@@ -9,15 +9,13 @@ public class SignupRequest {
     private String username;
 
     private String password;
-    private String passwordConfirmation;
 
     public SignupRequest() {
     }
 
-    public SignupRequest(String email, String username, String password, String passwordConfirmation) {
+    public SignupRequest(String email, String username, String password) {
         this.email = email;
         this.username = username;
         this.password = password;
-        this.passwordConfirmation = passwordConfirmation;
     }
 }
