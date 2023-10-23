c069d2e6f03762d09fc0f1a5e491b8628252d533
JaeBin <201924576@pusan.ac.kr>
2023-10-23 23:59:14 +0900

    feat : RefreshToken Entity 생성

--- backend/src/main/java/consolelog/auth/domain/RefreshToken.java
+++ backend/src/main/java/consolelog/auth/domain/RefreshToken.java
@@ -1,6 +1,31 @@
 package consolelog.auth.domain;
 
+import consolelog.support.token.InvalidRefreshTokenException;
+import jakarta.persistence.*;
+import lombok.Generated;
+import lombok.Getter;
+
+@Entity
+@Getter
 public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "refresh_token_id")
+    private Long id;
+
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "token")
+    private String token;
+
     public RefreshToken(Long memberId, String token) {
+        this.memberId = memberId;
+        this.token = token;
+    }
+
+    public void validateSameToken(String token) {
+        if (!this.token.equals(token))
+            return new InvalidRefreshTokenException();
     }
 }
