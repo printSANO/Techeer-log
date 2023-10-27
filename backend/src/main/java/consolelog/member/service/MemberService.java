db4fb784ccc2269f26a7cbd651ff50427f25d88b
Kiminni <imk0980@gmail.com>
2023-10-27 19:41:46 +0900

    fix: 닉네임 추가 및 추가 기능 수정

--- backend/src/main/java/consolelog/member/service/MemberService.java
+++ backend/src/main/java/consolelog/member/service/MemberService.java
@@ -1,9 +1,8 @@
 package consolelog.member.service;
 
-import consolelog.member.domain.Email;
-import consolelog.member.domain.Member;
-import consolelog.member.domain.Password;
-import consolelog.member.domain.Username;
+import consolelog.member.domain.*;
+import consolelog.member.dto.EditNicknameRequest;
+import consolelog.member.dto.NicknameResponse;
 import consolelog.member.dto.SignupRequest;
 import consolelog.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
@@ -19,15 +18,28 @@ public class MemberService {
         this.memberRepository = memberRepository;
     }
 
+    // Encryptor 추가해야함
     @Transactional
     public void signUp(SignupRequest signupRequest) {
         Member member = Member.builder()
-                .id(1L)
                 .username(new Username(signupRequest.getUsername()))
                 .password(new Password(signupRequest.getPassword()))
                 .email(new Email(signupRequest.getEmail()))
+                .nickname(new Nickname(signupRequest.getNickname()))
                 .build();
         memberRepository.save(member);
     }
+    // else throw 넣어야 함
+    @Transactional
+    public void editNickname(EditNicknameRequest editNicknameRequest, AuthInfo authInfo) {
+        Member member = memberRepository.findById(authInfo.getId())
 
+        Nickname validNickname = new Nickname(editNicknameRequest.getNickname());
+        member.updateNickname(validNickname);
+    }
+
+    public NicknameResponse findNickname(AuthInfo authInfo) {
+        Member member = memberRepository.findById(authInfo.getId());
+        return NicknameResponse.of(member);
+    }
 }
