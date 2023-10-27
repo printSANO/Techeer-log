a04be3f60fab44efd93d1da2c6bcb86e7633f8d3
Kiminni <imk0980@gmail.com>
2023-10-28 02:20:22 +0900

    feat: 유일여부 확인 기능 추가 및 변수명 변경

--- backend/src/main/java/consolelog/member/repository/MemberRepository.java
+++ backend/src/main/java/consolelog/member/repository/MemberRepository.java
@@ -1,11 +1,20 @@
 package consolelog.member.repository;
 
 import consolelog.member.domain.Member;
+import consolelog.member.domain.Nickname;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findById(Long id);
+
+    boolean existsMemberByLoginIDValue(String username);
+
+    boolean existsMemberByNickname(Nickname nickname);
+
+    boolean existsMemberByNicknameValue(String nickname);
 }
