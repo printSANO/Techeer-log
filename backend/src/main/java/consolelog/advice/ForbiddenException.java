e93b06e62c8bfb3645bf08357ee181805c9d644e
fallkim <fs728kimgc@naver.com>
2023-11-03 03:55:53 +0900

    fix : application.yml 수정

--- backend/src/main/java/consolelog/advice/ForbiddenException.java
+++ backend/src/main/java/consolelog/advice/ForbiddenException.java
@@ -1,10 +1,7 @@
 package consolelog.advice;
 
 public class ForbiddenException extends BusinessException {
-<<<<<<<<< Temporary merge branch 1
 
-=========
->>>>>>>>> Temporary merge branch 2
     public ForbiddenException(String message) {
         super(message);
     }
--- backend/src/main/java/consolelog/auth/exception/AuthorizationException.java
+++ backend/src/main/java/consolelog/auth/exception/AuthorizationException.java
@@ -3,10 +3,6 @@ package consolelog.auth.exception;
 import consolelog.advice.ForbiddenException;
 
 public class AuthorizationException extends ForbiddenException {
-<<<<<<< HEAD
-=======
-
->>>>>>> #8
     private static final String MESSAGE = "권한이 없습니다.";
 
     public AuthorizationException() {
