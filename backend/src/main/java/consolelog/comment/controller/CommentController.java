248191fb52f87574102f8a690c2fd0944120a2d4
fallkim <fs728kimgc@naver.com>
2023-11-04 00:41:54 +0900

    feat: comment-reply result response 반환 값 설정

--- backend/src/main/java/consolelog/comment/controller/CommentController.java
+++ backend/src/main/java/consolelog/comment/controller/CommentController.java
@@ -10,8 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-
 import static consolelog.result.ResultCode.*;
 
 
@@ -28,26 +26,27 @@ public class CommentController {
     public ResponseEntity<ResultResponse<CommentResponse>> addComment(@PathVariable(name = "id") Long postId,
                                                                       @Valid @RequestBody NewCommentRequest newCommentRequest,
                                                                       @Login AuthInfo authInfo) {
-        Long commentId = commentService.addComment(postId, newCommentRequest, authInfo);
-        ResultResponse<CommentResponse> resultResponse = new ResultResponse<CommentResponse>(COMMENT_CREATED_SUCCESS);
+        commentService.addComment(postId, newCommentRequest, authInfo);
+        ResultResponse<CommentResponse> resultResponse = new ResultResponse<>(COMMENT_CREATED_SUCCESS);
         return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
     }
 
     //대댓글
     @PostMapping("/comments/{id}/reply")
-    public ResponseEntity<Void> addReply(@PathVariable(name = "id") Long commentId,
-                                         @Valid @RequestBody NewReplyRequest newReplyRequest,
-                                         @Login AuthInfo authInfo) {
-        Long replyId = commentService.addReply(commentId, newReplyRequest, authInfo);
-        return ResponseEntity.created(URI.create("/comments/" + replyId)).build();
+    public ResponseEntity<ResultResponse<CommentResponse>> addReply(@PathVariable(name = "id") Long commentId,
+                                                                    @Valid @RequestBody NewReplyRequest newReplyRequest,
+                                                                    @Login AuthInfo authInfo) {
+        commentService.addReply(commentId, newReplyRequest, authInfo);
+        ResultResponse<CommentResponse> resultResponse = new ResultResponse<>(COMMENT_REPLY_CREATED_SUCCESS);
+        return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
     }
 
 
     @GetMapping("/posts/{id}/comments")
     public ResponseEntity<ResultResponse> findComments(@PathVariable(name = "id") Long postId,
                                                        @Login AuthInfo authInfo) {
-        CommentsResponse commentsResponse = commentService.findComments(postId, authInfo);
-        ResultResponse<Comment> resultResponse = new ResultResponse<Comment>(GET_COMMENT_SUCCESS);
+        commentService.findComments(postId, authInfo);
+        ResultResponse<Comment> resultResponse = new ResultResponse<>(GET_COMMENT_SUCCESS);
         return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
     }
 
@@ -55,8 +54,8 @@ public class CommentController {
     public ResponseEntity<ResultResponse<CommentResponse>> updateComment(@PathVariable(name = "id") Long commentId,
                                                                          @Valid @RequestBody UpdateCommentRequest updateCommentRequest,
                                                                          @Login AuthInfo authInfo) {
-        CommentResponse updatedComment = commentService.updateComment(commentId, updateCommentRequest, authInfo);
-        ResultResponse<CommentResponse> resultResponse = new ResultResponse<CommentResponse>(UPDATE_COMMENT_SUCCESS);
+        commentService.updateComment(commentId, updateCommentRequest, authInfo);
+        ResultResponse<CommentResponse> resultResponse = new ResultResponse<>(UPDATE_COMMENT_SUCCESS);
         return ResponseEntity.status(HttpStatus.OK).body(resultResponse);
     }
 
--- backend/src/main/java/consolelog/result/ResultCode.java
+++ backend/src/main/java/consolelog/result/ResultCode.java
@@ -7,9 +7,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResultCode {
     COMMENT_CREATED_SUCCESS("C001", "201", "댓글이 정상적으로 등록되었습니다."),
-    UPDATE_COMMENT_SUCCESS("C002", "201", "댓글이 정상적으로 수정되었습니다."),
-    DELETE_COMMENT_SUCCESS("C003", "200", "댓글이 정상적으로 삭제되었습니다."),
-    GET_COMMENT_SUCCESS("C004", "200", "댓글이 정상적으로 출력되었습니다.");
+    COMMENT_REPLY_CREATED_SUCCESS("C002", "201", "대댓글이 정상적으로 등록되었습니다."),
+    UPDATE_COMMENT_SUCCESS("C003", "201", "댓글이 정상적으로 수정되었습니다."),
+    DELETE_COMMENT_SUCCESS("C004", "200", "댓글이 정상적으로 삭제되었습니다."),
+    GET_COMMENT_SUCCESS("C005", "200", "댓글이 정상적으로 출력되었습니다.");
 
     private final String code;
     private final String status;
