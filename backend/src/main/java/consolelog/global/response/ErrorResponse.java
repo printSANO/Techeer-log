d7b0d838cd7482b9a04fc874a661d1b1c5db3303
JaeBin <201924576@pusan.ac.kr>
2024-03-25 17:21:11 +0900

    fix: response에서 staus 제거

--- backend/src/main/java/consolelog/global/response/ErrorResponse.java
+++ backend/src/main/java/consolelog/global/response/ErrorResponse.java
@@ -7,14 +7,12 @@ import lombok.Getter;
 @Getter
 public class ErrorResponse {
     private final String code;
-    private final int status;
     private final String message;
 
 
     @JsonCreator
     public ErrorResponse(ErrorCode errorCode) {
         this.code = errorCode.getCode();
-        this.status = errorCode.getStatus();
         this.message = errorCode.getMessage();
     }
 }
