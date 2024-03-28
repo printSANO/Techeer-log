393466946b11deacf655fd2b419a49892c554499
JaeBin <201924576@pusan.ac.kr>
2024-03-28 19:41:29 +0900

    fix: exception extends 오류 해결

--- backend/src/main/java/consolelog/global/support/token/InvalidAccessTokenException.java
+++ backend/src/main/java/consolelog/global/support/token/InvalidAccessTokenException.java
@@ -1,9 +1,9 @@
 package consolelog.global.support.token;
 
-import consolelog.global.exception.UnauthorizedException;
+import consolelog.global.exception.BusinessException;
 import consolelog.global.response.ErrorCode;
 
-public class InvalidAccessTokenException extends UnauthorizedException {
+public class InvalidAccessTokenException extends BusinessException {
 
     public InvalidAccessTokenException() {
         super(ErrorCode.INVALID_ACCESS_TOKEN_ERROR);
--- backend/src/main/java/consolelog/global/support/token/InvalidRefreshTokenException.java
+++ backend/src/main/java/consolelog/global/support/token/InvalidRefreshTokenException.java
@@ -1,9 +1,9 @@
 package consolelog.global.support.token;
 
-import consolelog.global.exception.UnauthorizedException;
+import consolelog.global.exception.BusinessException;
 import consolelog.global.response.ErrorCode;
 
-public class InvalidRefreshTokenException extends UnauthorizedException {
+public class InvalidRefreshTokenException extends BusinessException {
 
     public InvalidRefreshTokenException() {
         super(ErrorCode.INVALID_REFRESH_TOKEN_ERROR);
