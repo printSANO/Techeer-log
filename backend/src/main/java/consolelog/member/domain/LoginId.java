e150eb11c88b569397988b8352f5f43eeb59c5b8
JaeBin <201924576@pusan.ac.kr>
2024-04-08 17:35:25 +0900

    fix: 불필요한 equals 함수 제거

--- backend/src/main/java/consolelog/member/domain/LoginId.java
+++ backend/src/main/java/consolelog/member/domain/LoginId.java
@@ -1,13 +1,8 @@
 package consolelog.member.domain;
 
-import consolelog.auth.domain.encryptor.EncryptorI;
-import consolelog.member.exception.InvalidLoginIdException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
-
-import java.util.regex.Pattern;
-
 @Getter
 @Embeddable
 public class LoginId {
@@ -22,21 +17,4 @@ public class LoginId {
     public LoginId(String value) {
         this.value = value;
     }
-
-    // 수정 필요
-    // @override 가 빠졌고, equals를 상속하면 반드시 hashcode 도 상속해서 구현해주어야함
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        LoginId loginId = (LoginId) o;
-        return getValue().equals(loginId.getValue());
-    }
-
-
 }
