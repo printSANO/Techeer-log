1e1fcd137c093dcc6b60596514009e73f9abba29
JaeBin <201924576@pusan.ac.kr>
2024-04-15 17:00:55 +0900

    feat: Framework, Member Mapper 생성

--- backend/src/main/java/consolelog/global/config/MapperConfig.java
+++ backend/src/main/java/consolelog/global/config/MapperConfig.java
@@ -1,5 +1,9 @@
 package consolelog.global.config;
 
+import consolelog.framework.dto.FrameworkMapper;
+import consolelog.framework.dto.FrameworkMapperImpl;
+import consolelog.member.dto.MemberMapper;
+import consolelog.member.dto.MemberMapperImpl;
 import consolelog.project.dto.ProjectMapper;
 import consolelog.project.dto.ProjectMapperImpl;
 import org.springframework.context.annotation.Bean;
@@ -10,5 +14,13 @@ public class MapperConfig {
     @Bean
     public ProjectMapper projectMapper() {
         return new ProjectMapperImpl();
+    }
+
+    @Bean
+    public MemberMapper memberMapper() {return new MemberMapperImpl();
+    }
+
+    @Bean
+    public FrameworkMapper frameworkMapper() { return new FrameworkMapperImpl();
     }
 }
