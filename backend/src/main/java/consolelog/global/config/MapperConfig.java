646b94451f009ed026c400375ee4c7e3f47b6e55
JaeBin <201924576@pusan.ac.kr>
2024-04-17 16:05:07 +0900

    fix: MapperConfig 오류 수정

--- backend/src/main/java/consolelog/global/config/MapperConfig.java
+++ backend/src/main/java/consolelog/global/config/MapperConfig.java
@@ -1,11 +1,6 @@
 package consolelog.global.config;
 
-import consolelog.global.mapper.FrameworkMapper;
-import consolelog.framework.dto.FrameworkMapperImpl;
-import consolelog.global.mapper.MemberMapper;
-import consolelog.member.dto.MemberMapperImpl;
-import consolelog.global.mapper.ProjectMapper;
-import consolelog.project.dto.ProjectMapperImpl;
+import consolelog.global.mapper.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
