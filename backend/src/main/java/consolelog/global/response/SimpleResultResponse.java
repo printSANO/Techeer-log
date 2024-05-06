582b539462353c3e2c02379fb24831a8ea61a7fc
Kiminni <imk0980@gmail.com>
2024-05-07 00:07:23 +0900

    feat: SimpleResultResponse 추가

---
+++ backend/src/main/java/consolelog/global/response/SimpleResultResponse.java
@@ -0,0 +1,17 @@
+package consolelog.global.response;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
+
+@Getter
+public class SimpleResultResponse<T> {
+    private final String code;
+    private final String message;
+
+
+    @JsonCreator
+    public SimpleResultResponse(ResultCode resultCode) {
+        this.code = resultCode.getCode();
+        this.message = resultCode.getMessage();
+    }
+}
