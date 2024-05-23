62068c6084e6df19c584e6619fb391ec8cc79e23
youlimsongs <giuhki@gmail.com>
2024-05-23 19:58:42 +0900

    feat: token 받기 수정

--- frontend/src/shared/authorization/getToken.ts
+++ frontend/src/shared/authorization/getToken.ts
@@ -2,7 +2,5 @@
 // import useStore from './useStore'
 // import { useTokenStore } from './stores/useTokenStore'
 // const token = useStore(useTokenStore, (state) => state.bears)
-
-// export const accessToken = localStorage.getItem('accessToken');
-export const accessToken =
-'eyJhbGciOiJIUzUxMiJ9.eyJpZCI6MSwidHlwZSI6IlVTRVIiLCJuaWNrbmFtZSI6InN0cmluZyIsImlhdCI6MTcxNjQ2MDg1MiwiZXhwIjoxNzE2NDY0NDUyfQ.N5P6095fAAe9cJVmG2LAaQO9fe2MP_BT7ULWqBExqS7WR3i4v5ckEKzFJ4XrBJvsZpdCCyovtnVad6kOB_WTVw'
+const token = localStorage.getItem('accessToken');
+export const accessToken = token? token.split(' ')[1]: '';
