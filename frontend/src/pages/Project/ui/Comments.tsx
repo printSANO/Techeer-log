6470b9c4d293d5f7ae9b85cea9632f550199400a
youlimsongs <giuhki@gmail.com>
2024-06-20 23:28:09 +0900

    fix: 주석 삭제, navbar css 수정

--- frontend/src/pages/Project/ui/Comments.tsx
+++ frontend/src/pages/Project/ui/Comments.tsx
@@ -1,14 +1,7 @@
-//usestate같은거는 pages에서 씀
 import { useQuery } from '@tanstack/react-query';
 import { getComments, ProjectComment } from '../../../entities/ProjectComment';
 import { CommentData } from '../../../shared/types/comments.ts';
 
-//projectId는 useRef로 url에서 받아올거임
-// interface Comment {
-//   projectId: number;
-//   accessToken: string; zustand 전역상태관리로 스토리지 저장값 받아올거임
-//   content: string;
-// }
 export const Comments = ({ projectId }: { projectId: number }) => {
   const { data, isError, error, isLoading } = useQuery<CommentData>({
     queryKey: ['comments'],
@@ -22,8 +15,6 @@ export const Comments = ({ projectId }: { projectId: number }) => {
   if (isError) {
     return <div>Error: {error.message}</div>;
   }
-
-  // console.log('getcomment', data);
 
   if (data) {
     return (
